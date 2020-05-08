db653069ffc6a52f50ca736cb3370e9ca10b5408
Krystal <xinzi.wang@northwestern.edu>
2020-05-07 21:06:17 -0500

    final change

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,42 +1,45 @@
-// Creating map object
-var myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11
-  });
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  
+var mymap = L.map('map', {
+  center: [39.8283, -98.5795],
+  zoom: 2
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+maxZoom: 20,
+id: "mapbox.streets",
+accessToken: API_KEY
+}).addTo(mymap);
 
 
-// Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojsonn", createMarkers);
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
- 
-  
-  // Grab the data with d3
-  function createMarkers(data) {
+d3.json(link, function(data) {
+  createFeatures(data.features);
+});
+
+function createFeatures(data) {
+
+  function onEachFeature(feature, layer) {
+      layer.bindPopup ("<h2>Magnitude: " + feature.properties.mag);
+  }
 
 
-    // Loop through data
-    for (var i = 0; i < data.length; i++) {
 
-        // Set the data location property to a variable
-    var location = data[i];
+  var earth = L.geoJSON(data, {
+      onEachFeature: onEachFeature,
+      pointToLayer: function (feature, coordinate) {
+          var fill;
+        
+          var circles = {
+              radius: (5*(feature.properties.mag/Math.log(5))),
+              fillColor: fill,
+              color: "blue",
+              weight: 0.5,
+              fillOpacity: 0.25
+          };
 
-    // Check for location property
-    if (location) {
-        markers.addLayer(L.marker([data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]]).bindPopup(data[i].properties.mag));
+          return L.circleMarker(coordinate, circles);
       }
-    }
+  }).addTo(mymap);
   
-    // Add our marker cluster layer to the map
-    createMap(L.layerGroup(markers));
-  
-  };
-  
+};
